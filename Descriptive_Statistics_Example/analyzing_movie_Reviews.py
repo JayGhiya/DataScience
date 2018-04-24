b6b33af570b0426afcdb74cb2a567bab28de26ce
JayGhiya <ghiya6548@gmail.com>
2018-04-24 12:54:20 +0530

    Added more predictions

--- Descriptive_Statistics_Example/analyzing_movie_Reviews.py
+++ Descriptive_Statistics_Example/analyzing_movie_Reviews.py
@@ -3,13 +3,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats.stats import pearsonr as corr_p
+from scipy.stats.stats import pearsonr as corr
 from scipy.stats.stats import linregress as lr
 
-
-movies = pd.read_csv("give path to csv")
+movies = pd.read_csv("C:\\Users\\Jay Ghiya\\Documents\\DataQuest\\Statistics_Beginner_Project\\fandango_score_comparison.csv")
 print(movies.head(n=5))
-#performing statistical analysis 
+#performing statistical analysis
 #let us look at frequency distribution of metacritic vs fandango
 plt.hist(movies['Metacritic_norm_round'])
 plt.show()
@@ -38,12 +37,12 @@ print("Fandango std",Fandango_std)
     Regarding, mean fandango has a higher mean and median.! 
     let us find corelation between columns with help of scatter plot
 also if we subtract metacritic and fandango ratings, we should be able 
-   movies with the largest difference which will help us in finding the outliers""" 
+   movies with the largest difference which will help us in finding the outliers"""
 #scatter plot
 plt.scatter(movies["Fandango_Stars"],movies["Metacritic_norm_round"])
-plt.show()   
+plt.show()
 """ observation from scatter plot: several movies appear to be low rated in
-metacritic but has a high rating in Movie Reviews! let us explore this further"""   
+metacritic but has a high rating in Movie Reviews! let us explore this further"""
 #calculating difference between fandango and metacritic columns
 movies["fm_diff"] = abs(movies["Metacritic_norm_round"] - movies["Fandango_Stars"])
 sort_df = movies.sort_values(by=['fm_diff'],ascending=False)
@@ -54,11 +53,22 @@ This can be done by finding the corerelation coefficeint first and then we can a
 predict ratings of fandango based on metacritic if those two have a good corerelation coefficient
 So, without further due let us get started"""
 #calculating corerelation coeefficient first
-corr_ratings,p = corr_p(movies["Fandango_Stars"],movies["Metacritic_norm_round"])
+corr_ratings,p = corr(movies["Fandango_Stars"], movies["Metacritic_norm_round"])
+print(type(corr_ratings))
 print("corrrelation betwen fandango and metacriticis",corr_ratings)
-#let us use linear regression to do prediction of fandango rating based on metacritic rating
-slope, intercept, r_value, p_value, stderr_slope=lr(movies["Metacritic_norm_round"],movies["Fandango_Stars"])
-pred_3 = 3* slope + intercept
-print(pred_3)
-
+"""observation  based on corerelation coefficient is it is quite low """
+slope,intercept,rvalue,pvalue,stderr = lr(movies["Metacritic_norm_round"],movies["Fandango_Stars"])
+#as we have the slope and intercept we should be able to predict the fandango rating based on metacritic
+fandango_Rating_prediction_1 = slope * (3.0) + intercept
+print(fandango_Rating_prediction_1)
+#let us create a residual plot to better visualize metacritic and fandango lines
+fandango_Rating_prediction_2 = slope * (1.0) + intercept
+fandango_Rating_prediction_3 = slope * (5.0) + intercept
+plt.scatter(movies["Metacritic_norm_round"],movies["Fandango_Stars"])
+#plotting residuals
+list_x = [1.0,5.0]
+list_y = [fandango_Rating_prediction_2,fandango_Rating_prediction_3]
+plt.plot(list_x,list_y)
+plt.xlim(1,5)
+plt.show()
 
